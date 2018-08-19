d02d9475da73c4bba81cac480201ea3e96e7b1ad
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-19 01:07:04 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,6 @@
 def nyc_pigeon_organizer(data)
    pigeon_list = {}
 	  pigeon_names = []
-	  pigeon_names = []
   new_lvl_2_keys = data.keys #:color, :gender and :lives
   #collecting all names
   data.each do |key, value|
